a32d8a9766a2d721f3b296dc46efdc78208b823b
tredwood89 <tredwood89@gmail.com>
2017-10-09 00:01:08 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {jayz: "blueprint", nas: "rewind", biggie: "juicy"}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+
+  return Object.assign ( {}, playlist, {[artistName] : songTitle})
+}
+
+function removeFromPlaylist(playlist, artistName) {
+
+     return delete playlist.artistName;
+}
